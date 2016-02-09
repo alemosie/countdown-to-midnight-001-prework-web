d14ca581aa7b4a0959a65cddc5cee2f790f7e3c2
alemosie <alexandra.siega@gmail.com>
2016-02-09 11:43:08 -0500

    sleep(numSecs) until the new year

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(numSecs)
+    while numSecs > 0
+        puts "#{numSecs} SECOND(S)!"
+        numSecs-=1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(numSecs)
+    while numSecs > 0
+        sleep(1)
+        puts "#{numSecs} SECOND(S)!"
+        numSecs-=1
+    end
+    "HAPPY NEW YEAR!"
 end
